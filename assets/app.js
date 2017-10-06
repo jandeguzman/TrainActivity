e88f361f94c77e0a1a915cb3161ccfd05df05dd4
Janna de guzman <jandeguzman@Jannas-MacBook-Pro.local>
2017-10-06 00:31:12 -0700

    updates 2

--- assets/app.js
+++ assets/app.js
@@ -1,82 +1,72 @@
+$(document).ready(function() {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCa_uVcN5vY_CeikZckluqjrvx2mHd8yRg",
-    authDomain: "week-7-hw-6834a.firebaseapp.com",
-    databaseURL: "https://week-7-hw-6834a.firebaseio.com",
-    projectId: "week-7-hw-6834a",
-    storageBucket: "week-7-hw-6834a.appspot.com",
-    messagingSenderId: "182390229446"
-  };
-  firebase.initializeApp(config);
+    // var trainData = new Firebase("https://week-7-hw-6834a.firebaseio.com");
 
-  var database = firebase.database();
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCa_uVcN5vY_CeikZckluqjrvx2mHd8yRg",
+        authDomain: "week-7-hw-6834a.firebaseapp.com",
+        databaseURL: "https://week-7-hw-6834a.firebaseio.com",
+        projectId: "week-7-hw-6834a",
+        storageBucket: "week-7-hw-6834a.appspot.com",
+        messagingSenderId: "182390229446"
+    };
+    firebase.initializeApp(config);
 
-    var updateTime = function(){
-    var now = moment().format('hh:mm');
-    $('#currentTime').html(now);
-  }
+    var database = firebase.database();
 
-  $(document).ready(function(){
-    updateTime();
-    setInterval(updateTime, 1000);
-});
+    var destination = "";
+    var location = "";
+    var firstTrain = "";
+    var frequency = "";
 
+    $("#submit").on("click", function() {
 
-  $("#submit").on("click", function(){
+        destination = $("#destination").val().trim();
+        location = $("#location").val().trim();
+        firstTrain = $("#firstTrain").val().trim();
+        frequency = $("#frequency").val().trim();
 
+        var newData = {
+            destination: destination,
+            location: location,
+            firstTrain: firstTrain,
+            frequency: frequency,
+        }
 
-  var destination = $("#destination").val().trim();
-  var location = $("#location").val().trim();
-  var firstTrain = $("#firstTrain").val().trim();
-  var frequency = $("#frequency").val().trim();
+        database.ref().push(newData);
 
+        $("#destination").val("");
+        $("#location").val("");
+        $("#frequency").val("");
+        $("#firstTrain").val("");
 
-  var newData = {
-    destination: destination,
-    location: location,
-    firstTrain: firstTrain,
-    frequency: frequency
-  }
-
-  
-  database.ref().push(newData);
-
-  $("#destination").val("");
-  $("#location").val("");
-  $("#firstTrain").val("");
-  $("#frequency").val("");
+        // Prevents page from refreshing
+        return false;
+    });
 
 
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-  return false;
+        var value = childSnapshot.val()
 
-});
+        var destination = value.destination;
+        var location = value.location;
+        var firstTrain = value.firstTrain;
+        var frequency = value.frequency;
+
+    var diffTime = moment().diff(moment.unix(firstTrain), "minutes");
+    var timeRemainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency ;
+    var minutes = frequency - timeRemainder;
+
+    var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+    
+
+    // Append train info to table on page
+    $(".table > tbody").append("<tr><td>" + destination + "</td><td>" + location + "</td><td>"+ nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
 
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-  var destination = childSnapshot.val().destination;
-  var location = childSnapshot.val().location;
-  var firstTrain = childSnapshot.val().firstTrain;
-  var frequency = childSnapshot.val().frequency;
-
-
-  // var convertTime = moment(firstTrain, "hh:mm").subtract(1, "years");
-
-  // var currentTime = moment();
-
-  // var diffTime = moment().diff(moment(convertTime), "minutes");
-
-  // var tRemainder = diffTime % frequency;
-
-  // var tMinutesTillTrain = frequency - tRemainder;
-
-  // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  // var nextTrainConverted = moment(nextTrain).format("hh:mm");
-
-  $(".table > tbody").append("<tr><td>" + location + "</td><td>" + destination + "</td><td>"
-    + frequency + "</td><td>" + ""+ "</td><td>" + "" + "</td></tr>");
-
+    });
 
 });
